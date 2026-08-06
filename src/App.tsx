0bcab8bb07f06eef7771b390392a58272edfd469
Joshwa-N <njoshwa93@gmail.com>
2026-08-06 04:50:04 +0000

    Migrate from react-router-dom to react-router v8 to fix CVE

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router';
 import { Toaster } from 'sonner';
 import { CartProvider } from '@/context/CartContext';
 import { AuthProvider } from '@/context/AuthContext';
